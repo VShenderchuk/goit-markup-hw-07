d417312e41d0f0b5fe0ee5ef26e76e106521f3f4
VShenderchuk <64258673+VShenderchuk@users.noreply.github.com>
2020-07-31 20:46:37 +0300

    Fixes modalform

--- js/modal.js
+++ js/modal.js
@@ -1,36 +1,16 @@
-/*Fixed header*/
-    
-      /*
-        const { height: pageHeaderHeight } = document
-        .querySelector('*.page-header*') 
-        .getBoundingClientRect();
-        document.body.style.paddingTop = '${pageHeaderHeight}px';
-*/
-
-/*Script popup*/
- 
-const refs = {
-  openModalBtn: document.querySelector('[data-open-modal]'),
-  closeModalBtn: document.querySelector('[data-close-modal]'),
-  backdrop: document.querySelector('[data-modal]'),
-
-  
-
-  
-};
 
 
-refs.openModalBtn.addEventListener('click', toggleModal);
-refs.closeModalBtn.addEventListener('click', toggleModal);
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
 
-refs.backdrop.addEventListener('click', logBackdropClick);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
-function toggleModal() {
-  refs.backdrop.classList.toggle('is-hidden');
- 
-}
-
-
-function logBackdropClick() {
-  console.log('Это клик в бекдроп');
-}
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
